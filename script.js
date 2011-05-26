eff922c1fa20e91735c1d97dfab8dc4bd1bad270
AbusufiyanJ <abujafarullah1996@gmail.com>
2011-05-26 17:30:51 +0530

    code challenge 5

--- script.js
+++ script.js
@@ -372,7 +372,7 @@ else if(markk.bmi < johnn.bmi) {
 //-----------loops and iteration-------------
 
 //for loop
-/*
+
 for  (var i = 1; i <=20; i ++ ){
   console.log(i);
 }
@@ -389,7 +389,7 @@ while (i < john.length) {
     console.log(john[i]);
     i++;
 }
-*/
+
 // continue and break statements
 
 var john = ['john', 'smith' ,1990, 'designer' ,false ];
@@ -407,15 +407,36 @@ console.log(john[i]);
 // looping backwards
 for (var i = john.length - 1; i >= 0; i-- ){
   console.log(john[i]);
+};
+//code challenge 5
+var AAbu = {
+   fullnamee : 'Abu',
+   bills : [124, 48, 268, 180, 42], 
+   calctips : function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++ )
+    { 
+    var percentage;
+    var bill = this.bills[i];
+
+    if (bill <50) {
+    percentage = .2;
+    } else if (bill >=50 && bill < 200) {
+    percentage = .15;
+    } else {
+    percentage = .1;
+    }
+
+this.tips[i] =bill * percentage;
+this.finalValues[i] = bill * bill * percentage;
+
+  } 
+ }
 }
-
-
-
-
-
-
-
-
+AAbu.calctips();
+console.log(AAbu);
 
 
 
