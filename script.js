f67cd9ef1e09490a68510cc6a309d9c2e5f2cee1
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-02-12 15:27:57 +0530

    code challenge

--- script.js
+++ script.js
@@ -1,12 +1,89 @@
-var firstname = 'hello dubai yaw';
+/*
+var firstname = prompt('user name');
+
+var age = prompt("user age");
+ 
+console.log(firstname +' '+ age);
+
+var job,isMarried;
+
+job =  prompt('job role');;
+isMarried =  prompt('marriage status');;
+
+console.log('The user Name is '+firstname+' And his age is ' + age + ' his job is '
+ + job + ' married status '+ isMarried) ;
+
+age = 'twenty two';
+job = 'tester';
+
+ console.log('The user Name is '+firstname+' And his age is ' + age + ' his job is '
+ + job + ' married status '+ isMarried) ;
+  */
+
+  /*---operator----
+var year = 2019;
+var yearAbu = year - 22;
+
+var yearSufiyan = year - 23;
+console.log(typeof yearAbu);
+
+*/
+
+
+/*-----operator procedence--*/
+/*
+var now = 2018;
+var yearAbu = 1996;
+var fullAge = 18;
+
+var isFullage = now - yearAbu;
+var ageJohn = now -yearAbu;
+var agemark =35;
+var average = (ageJohn +agemark)/2;
+
+console.log(average);
+
+//mutiple assigments
+
+var x,y;
+
+x = y = (3+5) * 4 - 6;
+
+console.log(x,y);
+
+x *= 2;
+console.log(x);
+
+
+*/
+
+
+//code challange
+
+var massAbu = 95;
+var heightAbu =1.69;
+
+var massSufiyan =92;
+var heightSufiyan = 1.95;  
+   
+
+var BMIAbu = massAbu / (heightAbu * heightAbu);
+
+var BMISufiyan = massSufiyan / (heightSufiyan * heightSufiyan);
+
+console.log  ("the Abu BMI is  "+ BMIAbu 
++"   the Sufiyan BMI is " + BMISufiyan);
 
 
 
-console.log(firstname);
+ var markHigherBMI = BMIAbu < BMISufiyan;
+ console.log("Is Abu\'s BMI higher than sufiyan\'s? " + markHigherBMI);
 
-var lastname= true;
+ if  (BMIAbu > BMISufiyan) {
+  console.log("Is Abu\'s BMI higher than sufiyan\'s?")}
+ 
+  else{ 
+    console.log("Sufiyan higher than Abu")
+  };
+ 
 
-console.log (fullage);
-
-
-console.log(firstname+''+age);
