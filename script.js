199e332da4ca24f2bf7e71a0e67179f3487b5ac9
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-02-19 10:23:25 +0530

    added statements

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ console.log(x);
 
 
 //code challange
-
+/*
 var massAbu = 95;
 var heightAbu =1.69;
 
@@ -71,10 +71,7 @@ var BMIAbu = massAbu / (heightAbu * heightAbu);
 
 var BMISufiyan = massSufiyan / (heightSufiyan * heightSufiyan);
 
-console.log  ("the Abu BMI is  "+ BMIAbu 
-+"   the Sufiyan BMI is " + BMISufiyan);
-
-
+console.log  ("the Abu BMI is  "+ BMIAbu +"   the Sufiyan BMI is " + BMISufiyan);
 
  var markHigherBMI = BMIAbu < BMISufiyan;
  console.log("Is Abu\'s BMI higher than sufiyan\'s? " + markHigherBMI);
@@ -86,4 +83,53 @@ console.log  ("the Abu BMI is  "+ BMIAbu
     console.log("Sufiyan higher than Abu")
   };
  
+var Namee = "Abu";
+var age = 25 ;
+
+if (age < 13) {
+  console.log(Namee + ' is a boy ');
+} else if (age >= 13 && age < 18) {
+    console.log(Namee + 'is a teenage');
+} else {
+   console.log(Namee + ' is a man ');
+};
+
+*/ 
+
+var firstname , age;
+
+firstname = 'Abu';
+var age = 19;
+
+//Ternary Operator
+
+age <= 18 ? console.log(firstname+ ' Ride Cycle') : console.log(firstname+ '  You can drive bike');
+
+var drive = age < 18 ? ' cycle ' : 'bike';
+console.log(age + drive);
+
+/*
+if (age <= 18){
+  var drive = 'ride cycle';
+}else {
+var drive = ' ride bike';
+}
+
+console.log(age + drive); */
+
+//switch Statement
+/*
+var job = 'Driver';
+switch(job) {
+  case 'faculty':
+  console.log(firstname + ' is a teacher he teach how to code ');
+  break;
+  case 'Driver':
+  console.log(firstname + ' is a driver ');
+  break;
+  default :
+  console.log(firstname + ' he is doing some other job');
+}
+
+*/
 
