574b4c33bde2af48e437fd7b1b152bec7679bb51
AbusufiyanJ <abujafarullah1996@gmail.com>
2019-03-26 17:08:26 +0530

    added a function

---
+++ how js works/script.js
@@ -0,0 +1,81 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+var age = prompt("Enter your date of birth and find your age:");
+calculateAge(age);
+
+function calculateAge(year) {
+    console.log(2019 - year);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
